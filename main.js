955d727500e0047ad22ee89dfbc9ab5e1d765135
Gamattowicz <p.romanczuk31@gmail.com>
2021-01-15 11:41:28 +0100

    create dataList

---
+++ main.js
@@ -0,0 +1,38 @@
+//Array with data image's source and title's text
+dataList = [{
+        img: "images/img1.jpg",
+        text: 'First picture'
+    },
+    {
+        img: "images/img2.jpg",
+        text: 'Second picture'
+    },
+    {
+        img: "images/img3.jpg",
+        text: 'Third picture'
+    },
+    {
+        img: "images/img4.jpg",
+        text: 'Forth picture'
+    },
+    {
+        img: "images/img5.jpg",
+        text: 'Fifth picture'
+    },
+    {
+        img: "images/img6.jpg",
+        text: 'Sixth picture'
+    },
+    {
+        img: "images/img7.jpg",
+        text: 'Seventh picture'
+    },
+    {
+        img: "images/img8.jpg",
+        text: 'Eighth picture'
+    },
+    {
+        img: "images/img9.jpg",
+        text: 'Ninth picture'
+    }
+];
